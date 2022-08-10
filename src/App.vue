2f8e8bc02693d8c71c2459efacfdf228ac4ffc31
yc-x <34796396+yc-x@users.noreply.github.com>
2022-08-10 12:27:55 -0700

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <the-header :title="title"></the-header>
+    <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/LearningResource/StoredResources.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+    components: {
+        StoredResources, TheHeader
+    },
+    data(){
+        return {
+            title: 'title!!',
+            storedResources: [
+                {
+                    id: '0',
+                    title: 'Official Guide',
+                    description: 'The official documentation.',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: '1',
+                    title: 'Google',
+                    description: 'Go to Google!',
+                    link: 'https://google.com',
+                }
+            ]
+        };
+    }
+}
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+        box-sizing: border-box;
+    }
+
+    html {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+        margin: 0;
+    }
+</style>
---
+++ src/components/LearningResource/StoredResources.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <resource-item 
+        v-for="resource in resources" 
+        :key="resource.id"
+        :title="resource.title" 
+        :description="resource.description"
+        :link="resource.link"
+        ></resource-item>
+    </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default{
+    props:['resources'],
+    components: {ResourceItem},
+    data(){
+        return {
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
